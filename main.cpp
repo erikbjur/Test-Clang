b4c8d706e7295ef023b04ae7f696b0685f890e0c
Erik Bjur <erik@bjurengineering.com>
2024-09-15 10:48:46 -0700

    Added ability to enter the index you want to test

--- main.cpp
+++ main.cpp
@@ -14,9 +14,13 @@ int main()
 {
   unsigned int nextID = 0;
   const unsigned int NUMBER_OF_OBJECTS = 1000000;
-  const unsigned int ELEMENT_INDEX_TO_FIND = 975000;
 
-  std::cout << "Creating the map..." << std::endl;
+  std::cout << "Enter the index of the object you want to find later (0 - 1000000):\n";
+  
+  unsigned int ELEMENT_INDEX_TO_FIND = 0;
+  std::cin >> ELEMENT_INDEX_TO_FIND;
+
+      std::cout << "Creating the map..." << std::endl;
 
   // Create a new unordered map
   std::unordered_map<unsigned int, std::unique_ptr<MyApp::Entity>> DataStorage_unordered_map;
@@ -88,8 +92,9 @@ int main()
   // Now lets find an object in the vector
   start_time = std::chrono::high_resolution_clock::now();
   // Use ranges to find if an element with the target int value exists
-  auto iterator = std::ranges::find_if( DataStorage_vector,
-                                        []( const auto& obj ) { return obj->ObjectID == ELEMENT_INDEX_TO_FIND; } );
+  auto iterator = std::ranges::find_if( DataStorage_vector, [ELEMENT_INDEX_TO_FIND]( const auto& obj ) {
+    return obj->ObjectID == ELEMENT_INDEX_TO_FIND;
+  } );
   end_time = std::chrono::high_resolution_clock::now();
   time_in_ms = end_time - start_time;
 
@@ -105,13 +110,13 @@ int main()
   start_time = std::chrono::high_resolution_clock::now();
   iterator =
       std::find_if( DataStorage_vector.begin(), DataStorage_vector.end(),
-                    []( const auto& obj ) { return obj->ObjectID == ELEMENT_INDEX_TO_FIND; } );
+                    [ELEMENT_INDEX_TO_FIND]( const auto& obj ) { return obj->ObjectID == ELEMENT_INDEX_TO_FIND; } );
   end_time = std::chrono::high_resolution_clock::now();
   time_in_ms = end_time - start_time;
 
   if( iterator != DataStorage_vector.end() ) {
     const auto& object_from_vector = *iterator;
-    std::cout << "It took " << time_in_ms << " ms using std::find_if() to find" << object_from_vector->Name
+    std::cout << "It took " << time_in_ms << " ms using std::find_if() to find " << object_from_vector->Name
               << std::endl;
   } else {
     std::cout << "Element with object id " << ELEMENT_INDEX_TO_FIND << " not found." << std::endl;
@@ -124,7 +129,7 @@ int main()
   end_time = std::chrono::high_resolution_clock::now();
   time_in_ms = end_time - start_time;
   std::cout << "Done deleting elements!" << std::endl;
-  std::cout << "It took " << time_in_ms << " ms to delete the unordered_map\n";
+  std::cout << "It took " << time_in_ms << " ms to delete the vector\n";
   std::cout << "There are now " << DataStorage_vector.size() << " elements in the vector." << std::endl;
 
   return 0;
